8c62bcd111c5669685a3c95d7fef40b5c2f7f958
JD Long <jdlong@gmail.com>
2018-07-06 21:12:32 +0000

    rmarkdown (learnr) didn't work with turtles... so trying shiny and normal R docs

---
+++ turtle_action.R
@@ -0,0 +1,33 @@
+library(TurtleGraphics)
+
+fractal_tree <- function(s=100, n=2) {
+  if (n <= 1) {
+    turtle_forward(s)
+    turtle_up()
+    turtle_backward(s)
+    turtle_down()
+  }
+  else {
+    turtle_forward(s)
+    a1 <- runif(1, 10, 60)
+    turtle_left(a1)
+    fractal_tree(s*runif(1, 0.25, 1), n-1)
+    turtle_right(a1)
+    a2 <- runif(1, 10, 60)
+    turtle_right(a2)
+    fractal_tree(s*runif(1, 0.25, 1), n-1)
+    turtle_left(a2)
+    turtle_up()
+    turtle_backward(s)
+    turtle_down()
+  }
+}
+set.seed(123)
+turtle_init(500, 600, "clip")
+turtle_do({
+  turtle_up()
+  turtle_backward(250)
+  turtle_down()
+  turtle_col("darkgreen")
+  fractal_tree(100, 16)
+})
